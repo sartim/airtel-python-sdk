7c1d3043f5d9806bde8cedddeec384e3a2e9fdb8
Tim <sarrtim@gmail.com>
2019-09-14 19:05:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup
 
-long_description = "Airtel API REST SDK with suscription, payments integration"
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(name='Airtel API SDK',
+setup(name='airtel-python-sdk',
       version='0.1',
-      description='M-Pesa API SDK',
+      description='Airtel Smart API SDK',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/sartim/airtel-python-sdk',
       author='sartim',
-      author_email='***REMOVED***',
       license='MIT',
-      packages=['mpesa'],
+      packages=['airtel'],
       install_requires=['requests', ],
-      dependency_links=['https://github.com/sartim/airtel-python-sdk/master#egg=package-1.0'],
       zip_safe=False
       )
